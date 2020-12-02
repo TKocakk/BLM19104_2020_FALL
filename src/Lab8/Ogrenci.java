17e8e689b944e0b7a28aaa9ca2164fa87d817da9
Tuğçe Koçak <72867097+TKocakk@users.noreply.github.com>
2020-12-02 15:54:23 +0300

    lab8

---
+++ src/Lab8/Ogrenci.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab8;
+
+/**
+ *
+ * @author Tugce Kocak
+ */
+public class Ogrenci {
+    String adi;
+    String soyadi;
+    int no;
+    String bölüm;
+    String aldıgıDers;
+    
+}
